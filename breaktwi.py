6bd4b6371eb62a948afdaac445651a3df85ebcb3
aeoe39950426 <aeoe39950426@outlook.com>
2015-06-17 20:24:27 +0900

    Create breaktwi.py

---
+++ breaktwi.py
@@ -0,0 +1,46 @@
+#coding:UTF-8
+
+
+import twitter
+import time
+
+
+CK = '#########################'                          # Consumer Key
+CS = '##################################################' # Consumer Secret
+AT = '##########-#######################################' # Access Token
+AS = '#############################################'      # Accesss Token Secert
+
+
+api=twitter.Api(consumer_key=CK,consumer_secret=CS,access_token_key=AT,access_token_secret=AS)
+
+#起動時
+print u'システム起動します。'
+before = 0
+after = 0
+
+#’この中’には自分のスクリーンネーム
+user = api.GetUser(screen_name='@')
+id = user.id
+
+while 1:
+    #ここも
+    user = api.GetUser(screen_name='@')
+    tweets = api.GetStatus(id)
+    before = user.statuses_count
+    time.sleep(60)
+    #しつこいようだがここも
+    user = api.GetUser(screen_name='@aeoe39950426')
+    after = user.statuses_count
+    diff = after-before
+    print u'ツイートした数:%d' % (diff)
+    if (diff) >= 5:
+        i = 0
+        #ここで削除
+        tweets = api.GetUserTimeline(id )
+        while 5 <= (diff-i):
+            api.DestroyStatus(tweets[i].id )
+            i = i + 1
+        print u'削除したゾ。'
+        
+    else:
+        print u'異常なし。'
